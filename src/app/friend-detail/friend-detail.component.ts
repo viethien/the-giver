52ebe92207e2cf6d4e1d184035762a9df9e0c119
niunani <niunani@Nhus-MacBook-Pro.local>
2019-02-04 13:27:52 -1000

    Include Input symbol and add hero input property

--- src/app/friend-detail/friend-detail.component.ts
+++ src/app/friend-detail/friend-detail.component.ts
@@ -1,10 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { Friend } from '../friend';
 
 @Component({
   selector: 'app-friend-detail',
   templateUrl: './friend-detail.component.html',
   styleUrls: ['./friend-detail.component.scss']
 })
+
+@Input() friend: Friend;
+
 export class FriendDetailComponent implements OnInit {
 
   constructor() { }
