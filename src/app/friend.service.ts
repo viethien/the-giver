259fdf3b061c6e2fd66a12732a09650e916845de
niunani <niunani@Nhus-MBP.lan>
2019-02-09 12:54:48 -1000

    Modify getFriends method to return Observable<Friend[]>

--- src/app/friend.service.ts
+++ src/app/friend.service.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Friend } from './friend';
 import { FRIENDS } from './mock-friends';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class FriendService {
 
-  getFriends(): Friend[] {
-    return FRIENDS;
+  getFriends(): Observable<Friend[]> {
+    return of(FRIENDS);
   }
 
   constructor() { }
