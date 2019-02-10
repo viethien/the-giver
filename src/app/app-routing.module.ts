f3f754db641946357632cb588f97ab19067cec9c
niunani <niunani@Nhus-MBP.lan>
2019-02-09 14:39:41 -1000

    Add friends component path to routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FriendsComponent } from './friends/friends.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'friends', component: FriendsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
