7c9440338a49922bff0e3d6d3fc61f498229e77a
niunani <niunani@Nhus-MBP.lan>
2019-02-09 23:41:20 -1000

    Add components to app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FriendsComponent } from './friends/friends.component';
 import { FriendDetailComponent } from './friend-detail/friend-detail.component';
+import { MessagesComponent } from './messages/messages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FriendsComponent,
-    FriendDetailComponent
+    FriendDetailComponent,
+    MessagesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
