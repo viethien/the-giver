9b8d2de6f294542b47162a8e6a81417c82a2548d
niunani <niunani@Nhus-MBP.lan>
2019-02-09 23:36:23 -1000

    Create getFriend function to extract friend id from route and goBack function using location

--- src/app/friend-detail/friend-detail.component.ts
+++ src/app/friend-detail/friend-detail.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
 import { Friend } from '../friend';
+import { FriendService } from '../friend.service';
 
 @Component({
   selector: 'app-friend-detail',
@@ -13,9 +16,27 @@ import { Friend } from '../friend';
 export class FriendDetailComponent implements OnInit {
   @Input() friend: Friend;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private friendService: FriendService,
+    private location: Location,
+  ) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.getFriend();
+  }
+
+  getFriend(): void {
+    //extract id from route
+    //route.snapshot is static image of the route info shortly after component created
+    //+ operater converts string to a number - don't forget route parameters are always strings
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.friendService.getFriend(id)
+        .subscribe(friend => this.friend = friend);
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
