ca12336ce395886d7febf0b3c65fadc1bb9427c0
niunani <niunani@Nhus-MBP.lan>
2019-02-09 21:59:20 -1000

    Add friendService parameter to dashboard constructor and call getFriends function in life cycle hook

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Friend } from '../friend';
+import { FriendService } from '../friend.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements OnInit {
 
-  constructor() { }
+export class DashboardComponent implements OnInit {
+  friends: Friend[] = [];
+
+  constructor(private friendService: FriendService) { }
 
   ngOnInit() {
+    this.getFriends();
+  }
+
+  getFriends(): void {
+    this.friendService.getFriends()
+      .subscribe(friends => this.friends = friends.slice(1,5));
   }
 
 }
