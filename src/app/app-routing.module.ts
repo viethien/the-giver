4397ed0db962912c03781fd924c9c8481354787d
niunani <niunani@Nhus-MBP.lan>
2019-02-09 23:40:41 -1000

    Add friend detail path to routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,11 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { FriendDetailComponent } from './friend-detail/friend-detail.component';
 
 const routes: Routes = [
-  { path: 'friends', component: FriendsComponent },
-  { path: 'dashboard', component: DashboardComponent },
-  //semi colon to indicate placeholder for a specific friend id
-  { path: 'detail/:id', component: FriendDetailComponent },
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'friends', component: FriendsComponent },
+  //colon to indicate placeholder for a specific friend id
+  { path: 'detail/:id', component: FriendDetailComponent },
 ];
 
 @NgModule({
