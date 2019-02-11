7b6620199893fcc9196721708e4add60a04d133d
niunani <niunani@Nhus-MBP.lan>
2019-02-10 17:50:40 -1000

    Add friends array to in memory data service

---
+++ src/app/in-memory-data.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Friend } from './friend';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InMemoryDataService implements InMemoryDbService {
+  createDb() {
+    const friends = [
+      { id: 11, name: 'Seepok' },
+      { id: 12, name: 'Nhimbo' },
+      { id: 13, name: 'Babby' },
+      { id: 14, name: 'Mamita' },
+      { id: 15, name: 'Jam' },
+      { id: 16, name: 'Ayden' },
+      { id: 17, name: 'Vurv' },
+      { id: 18, name: 'Eddie' },
+      { id: 19, name: 'Mas' },
+      { id: 20, name: 'Tom' },
+    ];
+    return { friends };
+  }
+
+  //friend id + 1
+  genId(friends: Friend[]): number {
+    return friends.length > 0 ? Math.max(...friends.map(friend => friend.id)) + 1 : 11;
+  }
+
+  constructor() { }
+}
