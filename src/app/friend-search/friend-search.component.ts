712503030e3eb887a1e5836f7c9ec58a6eddc718
niunani <niunani@Nhus-MBP.lan>
2019-02-11 11:21:57 -1000

    Assign search term to friends observable using debounce, distinctUntilChanged, and switchMap to reduce requests

--- src/app/friend-search/friend-search.component.ts
+++ src/app/friend-search/friend-search.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import {
+    debounceTime, distinctUntilChanged, switchMap
+} from 'rxjs/operators';
+import { Friend } from '../friend';
+import { FriendService } from '../friend.service';
 
 @Component({
   selector: 'app-friend-search',
   templateUrl: './friend-search.component.html',
   styleUrls: ['./friend-search.component.scss']
 })
+
 export class FriendSearchComponent implements OnInit {
+  friends$: Observable<Friend[]>;
+  private searchTerms = new Subject<string>();
 
-  constructor() { }
+  constructor(private friendService: FriendService) { }
 
-  ngOnInit() {
+  search(term: string): void {
+    this.searchTerms.next(term);
+  }
+
+  ngOnInit(): void {
+    this.friends$ = this.searchTerms.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap((term: string) =>
+        this.friendService.searchFriends(term)),
+      );
   }
 
 }
